7da14eb2ee1246c0ebd734ba6630955908b8ea1f
Imbirel <katelin.kzm@gmail.com>
2026-07-08 03:59:00 +0300

    feat(catalog): add page stub for car detail page

---
+++ src/app/features/catalog/pages/car-detail-page/car-detail-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-page',
+  imports: [],
+  templateUrl: './car-detail-page.html',
+  styleUrl: './car-detail-page.scss',
+})
+export class CarDetailPage {}
